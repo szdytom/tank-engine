ac15d560cd5ca7ec90cfb0c52deacde59340f571
szdytom <szdytom@163.com>
2020-11-03 13:00:45 +0800

    update config

--- shared/config.ts
+++ shared/config.ts
@@ -7,21 +7,21 @@ const Config = {
         width: 1000, 
     },
     tanks: {
-        max_speed: 5,
+        max_speed: 5.12,
         turn_speed: {
             tank: 180,
             radar: 1080,
             gun: 720,
         },
-        fire_speed: [40, 1500, 2000, 2500, 6000],
-        fire_too_much_damage: 0.05,
-        crash_damage: 0.01,
+        fire_speed: [40, 750, 950, 1240, 3500, 5500],
+        fire_too_much_damage: 0.1,
+        crash_damage: 0.05,
         size: 50,
         radar_size: 15,
     },
     bullet: {
-        damage: [0.005, 0.2, 0.25, 0.35, 0.75],
-        speed: 15.2,
+        damage: [0.005, 0.2, 0.25, 0.35, 0.75, 1.2],
+        speed: 20.48,
     },
 };
 
